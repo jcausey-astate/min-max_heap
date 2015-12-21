a9b0a9fc14ed2f3304074f3f1c2acfb4e1413b5a
jcausey-astate <jcausey@astate.edu>
2015-12-20 21:47:07 -0600

    Freshen docs.

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['bubble_5fup',['bubble_up',['../db/d41/namespace__mmheap.html#aa832caf8107f78258f7f2e5294480b41',1,'_mmheap']]],
-  ['bubble_5fup_5fmax',['bubble_up_max',['../db/d41/namespace__mmheap.html#a6510cbd9ec46c56b766d7841d9216b08',1,'_mmheap']]],
+  ['bubble_5fup',['bubble_up',['../db/d41/namespace__mmheap.html#ab2e9cdc91d81a3541295209df67f8d46',1,'_mmheap']]],
+  ['bubble_5fup_5fmax',['bubble_up_max',['../db/d41/namespace__mmheap.html#aa8b5f8b22c89c24763bfe3c08edce067',1,'_mmheap']]],
   ['bubble_5fup_5fmin',['bubble_up_min',['../db/d41/namespace__mmheap.html#a1202a815bc6a5ead35da79b7d3f954b0',1,'_mmheap']]]
 ];
